b5ad137b53e137bcd078577be58f5e1faac4df4f
Victor Almeida <jvassp@gmail.com>
2020-11-09 16:30:23 -0300

    contador

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import {useState} from 'react';
+import Helmet from 'react-helmet'
 
 function Home(){
     return (
         <div>
+            <Helmet>
+                <title>Teorema de Pitágoras</title>
+                <link rel="shortcut icon" href="https://image.winudf.com/v2/image1/Y29tLmluaXR6ZXJfZGV2LnRlb3JlbWFQaXRhZ29yYXNfaWNvbl8xNTQxMTAxMDU5XzA0Ng/icon.png?w=170&fakeurl=1" type="image/x-icon"/>
+            </Helmet>
+
             <h1>Contador</h1>
             <Contador />
         </div>
